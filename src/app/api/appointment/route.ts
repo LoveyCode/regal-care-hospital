bc51c52bddfe1a41f7363c73a394efaf78fc421e
Loveth <lovazodo@gmail.com>
2025-06-16 16:57:54 +0100

    adding cache control to fetchappointments in prod mode

--- src/app/api/appointment/route.ts
+++ src/app/api/appointment/route.ts
@@ -1,15 +1,20 @@
-import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
-import { NextResponse } from "next/server";
-// your function
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions';
+import { NextResponse } from 'next/server';
+ // adjust as needed
 
 export async function GET() {
   try {
     const data = await getRecentAppointmentList();
-    return NextResponse.json(data);
+    return NextResponse.json(data, {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    });
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
     return NextResponse.json({ error: String(error) }, { status: 500 });
   }
-  }
+}
