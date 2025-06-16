3b45b02aa13a273d93b8f355da5cfd85de5eed7c
Loveth <lovazodo@gmail.com>
2025-06-16 17:57:57 +0100

    refetching using useAppointment hook

--- src/components/ListAppointment.tsx
+++ src/components/ListAppointment.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { columns } from '@/components/table/columns';
-import { useAppointments } from '@/components/adminAppointment';
+import { useAppointments } from './useAppointments';
+
 
 
 export function ListAppointment() {
--- src/components/useAppointments.tsx
+++ src/components/useAppointments.tsx
@@ -1,12 +1,13 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { AppointmentStats } from "../../types/appwrite.type";
 
 export function useAppointments() {
   const [data, setData] = useState<AppointmentStats | null>(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  const fetchData = useCallback(() => {
+    setLoading(true);
     fetch("/api/appointment")
       .then((res) => res.json())
       .then((json) => {
@@ -17,6 +18,12 @@ export function useAppointments() {
         setLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    fetchData();
+    const interval = setInterval(fetchData, 5000); // Poll every 5 seconds
+    return () => clearInterval(interval);
+  }, [fetchData]);
 
   return { data, loading };
 }
