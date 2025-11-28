5628d0211c8856b7cd76627bac6ec112e2e49146
Loveth <lovazodo@gmail.com>
2025-11-28 16:30:50 +0100

    adding next url to get comments in comment client comment

--- src/components/BlogComponents/CommentsClient.tsx
+++ src/components/BlogComponents/CommentsClient.tsx
@@ -27,7 +27,7 @@ export default function CommentsClient() {
   const { data: comments, isLoading } = useQuery({
     queryKey: ["comments"],
     queryFn: async () => {
-      const res = await fetch(`/api/comments`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/comments`, {
         cache: "no-store",
       });
       if (!res.ok) throw new Error("Failed to load comments");
